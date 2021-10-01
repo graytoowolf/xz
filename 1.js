4ea22205d52ae01e02a766ace2d474fce865f2ed
graywolf <graywolf186@gmail.com>
2021-10-01 17:16:07 +0800

    z

--- 1.js
+++ 1.js
@@ -2,3 +2,7 @@
 [Script]
 cron "30 7 * * *" script-path=https://raw.githubusercontent.com/asd920/Auto-jd/main/jd_ddly.js tag=东东乐园
 */
+const $ = new Env('签到领现金');
+const notify = $.isNode() ? require('./sendNotify') : '';
+//Node.js用户请在jdCookie.js处填写京东ck;
+const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
